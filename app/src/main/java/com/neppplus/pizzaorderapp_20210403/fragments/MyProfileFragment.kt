7f1c849e4f774e1ea49a522a1a2bc59806a55a80
장혜진 <aslk3366zz@naver.com>
2021-04-03 15:55:57 +0900

    닉네임 수정 화면으로 이동

--- app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/MyProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.pizzaorderapp_20210403.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.pizzaorderapp_20210403.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
 
@@ -19,6 +21,12 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        editNicknameBtn.setOnClickListener {
+
+//            val myIntent = Intent(activity, )
+
+        }
 
 
     }
