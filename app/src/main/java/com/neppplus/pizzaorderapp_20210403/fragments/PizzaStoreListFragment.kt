c84b3c7506f8db691d45ddcb09b47e03eccf2bd4
장혜진 <aslk3366zz@naver.com>
2021-04-03 13:50:16 +0900

    메인화면 뷰페이저 어댑터 작성

--- app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/PizzaStoreListFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.pizzaorderapp_20210403.R
 
-class pizzaStoreListFragment : Fragment() {
+class PizzaStoreListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
